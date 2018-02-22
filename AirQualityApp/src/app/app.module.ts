2db44ddfb10f9d44fae5e3c35198e57140a2f83a
wouter <wouter.huygen@hotmail.be>
2018-02-22 16:52:55 +0100

    Routing Added + Dependencies bugfix

--- AirQualityApp/src/app/app.module.ts
+++ AirQualityApp/src/app/app.module.ts
@@ -4,23 +4,31 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { FloorPlanComponent } from './floor-plan/floor-plan.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FloorPlanComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MDBBootstrapModule.forRoot(),
-    ChartsModule
+    ChartsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: FloorPlanComponent},
+      { path: 'floorplan', component: FloorPlanComponent},
+      { path: '', redirectTo: 'home', pathMatch: 'full'}
+    ], { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent],

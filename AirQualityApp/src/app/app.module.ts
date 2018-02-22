05ce4ceaabff8426491546ae3620e558fc863135
wouter <wouter.huygen@hotmail.be>
2018-02-22 13:28:54 +0100

    Added Basic FloorPlan

--- AirQualityApp/src/app/app.module.ts
+++ AirQualityApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FloorPlanComponent } from './floor-plan/floor-plan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FloorPlanComponent
   ],
   imports: [
     BrowserModule,

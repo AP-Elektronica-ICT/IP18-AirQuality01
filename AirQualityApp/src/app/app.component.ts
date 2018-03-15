639c28dc5b84b164c7edc82149a696d439094ef3
wouter <wouter.huygen@hotmail.be>
2018-03-15 16:58:22 +0100

    Firebase stable

--- AirQualityApp/src/app/app.component.ts
+++ AirQualityApp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
@@ -10,5 +10,30 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user: Observable<firebase.User>;
+  items: AngularFireList<any[]>;
+  itemList: Observable<any[]>;
+  msgVal: string;
+
+  constructor(public angularFireAuth: AngularFireAuth, public angularFireDatabase: AngularFireDatabase) {
+    this.user = angularFireAuth.authState;
+    this.items = angularFireDatabase.list('items');
+    this.itemList = angularFireDatabase.list('items').valueChanges();
+    this.msgVal = '';
+  }
+
+  login() {
+    this.angularFireAuth.auth.signInAnonymously();
+    console.log(this.user);
+  }
+
+  logout() {
+    this.angularFireAuth.auth.signOut();
+  }
+
+  Send(desc: any) {
+    const descs = [];
+    descs[0] = desc;
+    this.items.push(descs);
+  }
 }

9c7e54b7986d98a26026c40b49d66617ad1dc7b9
Unknown <bryan_dv@hotmail.com>
2018-03-24 22:03:06 +0100

    IP05-78 able to login with google account

--- AirQualityApp/src/app/nav-bar/nav-bar.component.ts
+++ AirQualityApp/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,Input } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {AngularFireAuth} from "angularfire2/auth";
+import * as firebase from "firebase/app";
+import {Router} from "@angular/router";
+import {EventEmitter} from "events";
+import { auth } from 'firebase/app';
+import { window } from 'angular-bootstrap-md/utils/facade/browser';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +13,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+user:Observable<firebase.User>;
+authenticated: boolean = false;
 
-  constructor() { }
+  constructor(public af:AngularFireAuth, private router:Router) {
+    this.af.authState.subscribe(
+      (auth) => {
+        if(auth != null){
+          this.user = af.authState;
+          this.authenticated = true;
+        }
+      });
+   }
 
   ngOnInit() {
   }
+
+  login(){
+    this.af.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((result)=>{
+      this.authenticated = true;
+      console.log('Signed in successfully!');
+    }).catch((error)=>{
+      this.authenticated = false;
+      console.log('Error signing in: ',error);
+    })
+  }
+
+  logout() {
+    console.log('logout');
+   this.af.auth.signOut()
+   .then((result)=>{
+     this.router.navigate(['']).then(function(){
+      window.location.reload();
+       this.authenticated = false;
+     });
+     console.log('You were logged out successfully!');
+   }).catch((error) =>{
+     this.authenticated = true;
+     console.log('Error signing out: ',error);
+   })
+ }
 
 }

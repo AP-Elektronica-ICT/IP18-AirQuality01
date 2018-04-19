b37f432fff9c023f9ef9639ea0e9495f869ff1c4
Unknown <sander19_belmans@hotmail.com>
2018-04-20 01:26:25 +0200

    IP05-87
    Second Floorplan added and centered.

---
+++ AirQualityApp/src/app/floor-plan/mapTwo/mapTwo.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-mapTwo',
+  templateUrl: './mapTwo.component.html',
+  styleUrls: ['./mapTwo.component.css']
+})
+export class MapTwoComponent implements OnInit {
+/* TODO array aanmaken van kamers en colors zodat elke kamer zijn kleur krijgt*/
+
+
+color= "black"; // default waarde 
+//Binnenhalen van Value AirQuality en toewijzen aan Room
+//Hieronder nog fictieve waarden
+
+Room1 = new Room(41);
+
+  constructor() { }
+/* 
+AirQuality value verdelen in 5 groepen(nog fictieve waarden)
+  * 0  - 10 ver onder de goede waarde  ==> blauwe kleur
+  * 11 - 20 iets onder de goede waarde ==> blauwgroene kleur
+  * 21 - 30 op de goede waarde         ==> groene kleur
+  * 31 - 40 iets boven de goed waarde  ==> oranje kleur
+  * 41 - 50 ver boven de goede waarde  ==> rode kleur
+*/
+  ngOnInit() {
+    if((this.Room1.getValue() > 0)&&(this.Room1.getValue()<=10))
+    {
+      this.color = "#00bfff";
+    }    
+    if((this.Room1.getValue() > 10)&&(this.Room1.getValue()<=20))
+    {
+      this.color = "#00ffbf";
+    }
+    if((this.Room1.getValue() > 20)&&(this.Room1.getValue()<=30))
+    {
+      this.color = "#40ff00";
+    }
+    if((this.Room1.getValue() > 30)&&(this.Room1.getValue()<=40))
+    {
+      this.color = "#ffbf00";
+    }
+    if((this.Room1.getValue() > 40)&&(this.Room1.getValue()<=50))
+    {
+      this.color = "#ff0000";
+    }
+  }
+}
+
+ class Room{
+   value : number;
+   constructor(Value:number)
+   {
+     this.value = Value;
+   }
+   getValue()
+   {
+     return this.value;
+   }
+ }
